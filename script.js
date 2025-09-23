7959c0364f7fa6165cbe1ba322feb8b04aab3e98
Kirbo_16 <107632180+Kirbotheprogrammer@users.noreply.github.com>
2025-09-23 23:46:52 +0200

    Per te <3

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
+    var sixtseven = false
     let video = document.getElementById("image-container");
-    video.onclick = function(){
-        video.innerHTML = '<iframe width="1840" height="1035" src="https://www.youtube.com/embed/MIQ-iFrRIpg?autoplay=1" title="An Erasmus movie 2   The Spainlands" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>'
+    if (sixtseven == false){
+        video.onclick = function(){
+        document.getElementById("Andrea").className = "Alletto";
+        video.innerHTML = '<iframe class="Sigma" width="1175" height="661" src="https://www.youtube.com/embed/ozMUtZEEVSM" title="HO INSTALLATO IOS 26 SULL&#39;IPHONE 11 PRO DI 6 ANNI FA! 😳 L&#39;iPhone SUPPORTATO PIÙ VECCHIO! esplode?" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>';
+        sixtseven = true;
+        }
     }
+    if (sixtseven == true) {
+        console.log("Sigma, funziona");
+        video.onclick = function(){
+            console.log("Sigma, funziona");
+            video.innerHTML = '<img id="main-image" src="Click.png" alt="Immagine iniziale">';
+            sixtseven = false;
+    }
+}
         const targetDate = new Date("2025-09-24T23:59:59").getTime();
         function updateCountdown() {
             const now = new Date().getTime();
@@ -20,6 +33,8 @@
                 clearInterval(interval);
             }
         }
+
+    
 
         const interval = setInterval(updateCountdown, 1000);
         updateCountdown();
